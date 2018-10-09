e93dd05558f5e8155b17aa5332035690457f33c5
Prakash Bhattarai <prakash1bhattarai25@gmail.com>
2018-10-09 09:17:32 +0545

    code for sieve added

---
+++ sieve.cpp
@@ -0,0 +1,19 @@
+#define SIZE int(5e6)
+
+bool primes[SIZE];
+
+void sieve() {
+	for ( int i = 2; i < SIZE; i ++ ) {
+		if ( !primes[i] ) continue;
+		for ( int j = 2 * i; j < SIZE; j += i ) primes[j] = false;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	for ( int i = 0; i < SIZE; i ++ ) primes[i] = true;
+	sieve();
+	return 0;
+}
