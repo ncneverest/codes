17461b35a45e9ee17bdc0653ea81bcc78f2edbe2
Prakash Bhattarai <prakash1bhattarai25@gmail.com>
2018-10-09 10:11:14 +0545

    updated sieve code

--- sieve.cpp
+++ sieve.cpp
@@ -3,6 +3,7 @@
 bool primes[SIZE];
 
 void sieve() {
+	for ( int i = 0; i < SIZE; i ++ ) primes[i] = true;
 	for ( int i = 2; i < SIZE; i ++ ) {
 		if ( !primes[i] ) continue;
 		for ( int j = 2 * i; j < SIZE; j += i ) primes[j] = false;
@@ -13,7 +14,6 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	for ( int i = 0; i < SIZE; i ++ ) primes[i] = true;
 	sieve();
 	return 0;
 }
